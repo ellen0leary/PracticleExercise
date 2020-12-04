6765833d5f047afb70645c404813184a3b890d48
Ellen <eol50559@gmail.com>
2020-12-04 11:56:06 +0000

    Added  valadation to activites

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    //lists class
-    //list interface
-    //list of list interface
     public static ArrayList<ListActivity> allLists = new ArrayList<ListActivity>();
     private static Scanner read = new Scanner(System.in);
 
@@ -84,79 +81,97 @@ public class Main {
 
     }
     private static void addActivity() {
-        listAllLists();
-        System.out.println("Choose list to add to");
-        int i = read.nextInt();
+        if(checkAllLists()) {
+            listAllLists();
+            System.out.println("Choose list to add to");
+            int i = read.nextInt();
 
-        System.out.print(allLists.get(i).listActivities());
-        System.out.println("--------");
-        read.nextLine();
-        System.out.print("\nEnter name of activity : ");
-        String name = read.nextLine();
-
-
-        System.out.print("\nEnter description of activity : ");
-        String description = read.nextLine();
+            System.out.print(allLists.get(i).listActivities());
+            System.out.println("--------");
+            read.nextLine();
+            System.out.print("\nEnter name of activity : ");
+            String name = read.nextLine();
 
 
-        System.out.print("\nEnter tag for activity : ");
-        String tag = read.nextLine();
+            System.out.print("\nEnter description of activity : ");
+            String description = read.nextLine();
 
 
-        allLists.get(i).addActivities(name, description, tag);
-        allLists.get(i).listActivities();
+            System.out.print("\nEnter tag for activity : ");
+            String tag = read.nextLine();
+
+
+            allLists.get(i).addActivities(name, description, tag);
+            allLists.get(i).listActivities();
+        }
     }
 
     private static void listActivities() {
-        listAllLists();
-        System.out.println("Choose list to see to");
-        int i = read.nextInt();
-        System.out.print(allLists.get(i).listActivities());
+        if(checkAllLists()) {
+            listAllLists();
+            System.out.println("Choose list to see to");
+            int i = read.nextInt();
+            System.out.print(allLists.get(i).listActivities());
+        }
     }
 
     private static void updateActivity() {
-        listAllLists();
-        System.out.println("Choose list to see to");
-        int i = read.nextInt();
-        System.out.print(allLists.get(i).listActivities());
+        if(checkAllLists()) {
+            listAllLists();
+            System.out.println("Choose list to see to");
+            int i = read.nextInt();
+            System.out.print(allLists.get(i).listActivities());
 
-        System.out.println("Choose an activity to update");
-        int j = read.nextInt();
+            System.out.println("Choose an activity to update");
+            int j = read.nextInt();
 
-        read.nextLine();
+            read.nextLine();
 
-        System.out.println("Enter the new name");
-        String name = read.nextLine();
+            System.out.println("Enter the new name");
+            String name = read.nextLine();
 
-        System.out.println("Enter the new description");
-        String description = read.nextLine();
+            System.out.println("Enter the new description");
+            String description = read.nextLine();
 
-        System.out.println("Enter the new tag");
-        String tag = read.nextLine();
+            System.out.println("Enter the new tag");
+            String tag = read.nextLine();
 
-        allLists.get(i).updateActivity(j, name, description, tag);
+            allLists.get(i).updateActivity(j, name, description, tag);
+        }
     }
 
 
     private static void deleteActivity() {
-        listAllLists();
-        System.out.println("Choose list to see to");
-        int i = read.nextInt();
-        System.out.print(allLists.get(i).listActivities());
-        System.out.println("Choose an activity to delete");
-        int j = read.nextInt();
-        if(allLists.get(i).deleteActivity(j)) System.out.println("Activity deleted");
-        else System.out.println("activity not deleted");
+        if(checkAllLists()) {
+            listAllLists();
+            System.out.println("Choose list to see to");
+            int i = read.nextInt();
+            System.out.print(allLists.get(i).listActivities());
+            System.out.println("Choose an activity to delete");
+            int j = read.nextInt();
+            if (allLists.get(i).deleteActivity(j)) System.out.println("Activity deleted");
+            else System.out.println("activity not deleted");
+        }
     }
 
     private static void shuffleActivities() {
-        listAllLists();
-        System.out.println("Choose list to shuffle to");
-        int i = read.nextInt();
-        System.out.println("Shuffling list");
-        allLists.get(i).shuffle();
-        System.out.println("Start with:");
-        System.out.println(allLists.get(i).retrive(0).toString());
+        if (checkAllLists()) {
+            listAllLists();
+            System.out.println("Choose list to shuffle to");
+            int i = read.nextInt();
+            System.out.println("Shuffling list");
+            allLists.get(i).shuffle();
+            System.out.println("Start with:");
+            System.out.println(allLists.get(i).retrive(0).toString());
+        }
 
+    }
+
+    private static boolean checkAllLists(){
+        if(allLists.size() == 0){
+            System.out.println("Add ToDo list first");
+            return false;
+        }
+        return true;
     }
 }
