88b0c424622187eb61b441bd4f87f52248c98fce
ellen0leary <47063341+ellen0leary@users.noreply.github.com>
2020-12-04 09:31:01 +0000

    Initial

---
+++ src/List.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class List {
+    private ArrayList<Activity> activities = new ArrayList<Activity>(); //list of activities in a list
+    private String listName;
+
+
+
+}
